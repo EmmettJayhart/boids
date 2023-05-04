dd89f34337c366bca42969287422484ee84d1241
Emmett Jayhart <23242564+EmmettJayhart@users.noreply.github.com>
2023-05-04 03:14:48 +0100

    Debug inspector

--- src/ui.rs
+++ src/ui.rs
@@ -1,12 +1,16 @@
 use bevy::prelude::*;
-use bevy_boids::BoidDescriptor;
-use bevy_inspector_egui::quick::ResourceInspectorPlugin;
-// use bevy_inspector_egui::quick::WorldInspectorPlugin;
+#[cfg(feature = "dev")]
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+#[cfg(not(feature = "dev"))]
+use {bevy_boids::BoidDescriptor, bevy_inspector_egui::quick::ResourceInspectorPlugin};
 
 pub struct UiPlugin;
 impl Plugin for UiPlugin {
     fn build(&self, app: &mut App) {
+        #[cfg(not(feature = "dev"))]
         app.add_plugin(ResourceInspectorPlugin::<BoidDescriptor>::default());
-        // app.add_plugin(WorldInspectorPlugin);
+
+        #[cfg(feature = "dev")]
+        app.add_plugin(WorldInspectorPlugin);
     }
 }
